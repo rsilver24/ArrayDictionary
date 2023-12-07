56b153613ef0a7891fdc489d8fe0a9d3550f9e70
rsilver24 <rsilver24@kentdenver.org>
2023-12-07 09:50:44 -0700

    code organization and file creation

---
+++ ArrayDictionary/src/ArrayDictionary.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ArrayDictionary<T> {
+
+    // Instance Data
+
+    private ArrayList<T> keys;
+    private ArrayList<T> values;
+    private int size;
+
+    // Constructors
+
+    public ArrayDictionary(){
+        keys = new ArrayList<>();
+        values = new ArrayList<>();
+        size = 0;
+    }
+
+    // Methods
+
+    public void put(T key, T value){
+        
+    }
+
+    public T get(T key){
+
+    }
+
+    public T remove(T key){
+
+    }
+
+    public boolean contains(T key){
+
+    }
+
+    public boolean isEmpty(){
+
+    }
+
+    public int size(){
+
+    }
+
+    public Collection<T> keys(){
+
+    }
+
+    public Collection<T> values(){
+
+    }
+}
